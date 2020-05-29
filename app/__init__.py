6ab6ea5d8aba7434f4e56aecffa5ad906190ac36
ANASTANSIA <anastansiaserem@gmail.com>
2020-05-29 10:28:56 +0300

    Followers implementation

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 import logging
-from logging.handlers import SMTPHandler
+from logging.handlers import RotatingFileHandler, SMTPHandler
 
 
 # Creates application object as an instance of class Flask imported from Flask Package
@@ -25,6 +25,7 @@ login.init_app(app)
 login.login_view = 'login'
 
 from app import routes,models,errors 
+import os
 
 # workaround to circular imports
 
@@ -42,5 +43,17 @@ if not app.debug:
                 credentials=auth, secure=secure)
             mail_handler.setLevel(logging.ERROR)
             app.logger.addHandler(mail_handler)
+
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/microblog.log',maxBytes=10240,
+                       backupCount=10)
+        file_handler.setFormatter(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Microblog Application')
+
+
 
             
--- app/models.py
+++ app/models.py
@@ -15,7 +15,10 @@ def load_user(id): # id is a string, so the databases tha uses numeric ids need 
 #class inherits from db Model,base class for all models from FLAsk SQLalchemy
 
 #UserMixin is a Mixin class that include the generic implementattions that are appropriate for most user model classes
-
+  
+followers = db.Table('followers',db.Column('follower_id',db.Integer,db.ForeignKey('user.id')),
+            db.Column('followed_id',db.Integer,db.ForeignKey('user.id'))
+           )
 class User (UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64),index=True,unique=True)
@@ -44,9 +47,45 @@ class User (UserMixin,db.Model):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest,size)
 
+    # following methods
 
+    def follow(self,user):
+        if not self.is_following(user):
+            self.follow.append(user)
 
+    def unfollow(self,user):
+        if self.is_following(user):
+            self.followed.remove(user)
 
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+      
+
+    followed = db.relationship(
+        'User',secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers',lazy='dynamic'),lazy='dynamic'
+
+    )
+# Fetches posts belonging to the followers of the user ,in descending order
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())
+
+ 
+                
+            
+        
+##join()-- invoked on the post table(instructs the db to create a temporary table that combines data from posts and followers table )
+# first arg is the followers table
+# second is the join condition (followed_id of the followers table must be equal to the users_is  of the post table)
+#filter_by()..returns for a particular user
+#order_by().responsible for sorting in descending order
 class Post (db.Model):
     id=db.Column(db.Integer,primary_key=True)
     body = db.Column(db.String(140))
@@ -57,5 +96,4 @@ class Post (db.Model):
     def __repr__(self):
 
         return '<Post {}>'.format(self.body)
-
 
